ac31376fd4b791cdd6272a8df0f3484d6ebc3373
abhijeet <abhijeetwankhade2001@gmail.com>
2021-07-23 11:41:10 +0530

    add: proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,11 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-    app.use(proxy("/api/**", { target: "http://localhost:4000" }));
+    app.use(
+        "/api",
+        createProxyMiddleware({
+            target: "https://ab-server19.herokuapp.com",
+            changeOrigin: true,
+        })
+    );
 };
