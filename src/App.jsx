50108d451ff3d6e72dd38e5fb11febc8f5667b07
abhijeet <abhijeetwankhade2001@gmail.com>
2021-07-25 14:04:12 +0530

    removed react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,16 @@
-
-import React from 'react';
+import React from "react";
 import Main from "./Pages/Main";
-import { Switch, Route} from 'react-router-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from "@material-ui/core/CssBaseline";
 import "font-awesome/css/font-awesome.min.css";
 
-
-function App(props){
-    
+function App(props) {
     return (
-        <div className="body">
-
+        <div className='body'>
             <CssBaseline />
-            
-                <Switch>
-            
-                    <Route path='/' render={props => <Main />}
-                    />
 
-                </Switch>
-            
-            
+            <Main />
         </div>
     );
 }
 
-
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
-import {BrowserRouter as Router} from "react-router-dom";
 import "./mySASS/_main.scss";
 
-
-
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
